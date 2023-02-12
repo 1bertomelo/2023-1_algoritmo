0e58e56300c0bad20995e8db773fb5722cf4aa7c
betomelo_9 <betomelo_9@hotmail.com>
2023-02-12 10:32:16 -0300

    Aula02 exemplos

---
+++ 1Bimestre/aula02/exemplo01/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exemplo Lista 1!");
+List<int> listaInteiros = new List<int>();
+
+int qtdeIteiros = 5;
+
+for (int i = 0; i < qtdeIteiros; i++)
+{
+    Console.WriteLine("Digite um valor para inserir:");
+    int valor = Convert.ToInt32(Console.ReadLine());
+    listaInteiros.Add(valor);
+}
+
+Console.WriteLine("Impressão da lista!");
+foreach (var item in listaInteiros)
+{
+ Console.WriteLine($"Valor = {item}");   
+}
+
+
+Console.WriteLine("Digite um valor para buscar e remover:");
+int valorPesquisa = Convert.ToInt32(Console.ReadLine());
+
+if(listaInteiros.Contains(valorPesquisa)){
+    Console.WriteLine($"Valor {valorPesquisa} encontrado na posição {listaInteiros.IndexOf(valorPesquisa)} e removido!");
+    listaInteiros.Remove(valorPesquisa);
+}
+else {
+     Console.WriteLine($"Valor {valorPesquisa} não encontrado");
+}
+
+Console.WriteLine("Impressão da lista atualizada!");
+foreach (var item in listaInteiros)
+{
+ Console.WriteLine($"Valor = {item}");   
+}
---
+++ 1Bimestre/aula02/exemplo02/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exemplo 02!");
+List<string> listaNomes = new List<string>();
+
+int qtNomes = 5;
+
+for (int i = 0; i < qtNomes; i++)
+{
+    Console.WriteLine("Digite um nome para inserir na lista:");
+    string nome = Console.ReadLine();
+    listaNomes.Add(nome);
+}
+
+Console.WriteLine("Impressão lista");
+foreach (var item in listaNomes)
+{
+     Console.WriteLine($"Valor {item}");
+}
+
+Console.WriteLine("Digite um nome para pesquisar na lista:");
+string nomePesquisa = Console.ReadLine();
+
+if(listaNomes.Contains(nomePesquisa)){
+     Console.WriteLine($"O nome {nomePesquisa} encontrado na posição {listaNomes.IndexOf(nomePesquisa)} ");
+}
+else {
+     Console.WriteLine($"O nome {nomePesquisa} não encontrado {listaNomes.IndexOf(nomePesquisa)} ");
+}
